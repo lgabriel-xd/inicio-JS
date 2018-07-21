351b438e1d6537168d8311cd7f0321c4c1f1b44a
Gabriel Severiche Arancibia <lgabriel_xdl@hotmail.com>
2018-07-21 14:41:56 -0400

    mi primero commit

---
+++ primerscript.js
@@ -0,0 +1,25 @@
+// console.log para mostrar resultado en consola
+console.log('hola mundo');
+//declaracion de variables (varia el valor cuando uno requiera) y constantes (el valor queda constante) 
+var variable1;
+var variable3;
+var Variable;
+const arreglo1 = [10,5];
+const arreglo2 = [1,2];
+variable1 = 2+2;
+variable3 = 10;
+Variable  = 'Curso JS';
+console.log(variable1);
+Variable = null;
+console.log(Variable);
+console.log(arreglo1);
+console.info(variable1/3);
+console.log(variable1+variable3);
+console.log(variable1 +''+ variable3);
+console.log(variable1+variable3+''+Variable);
+// tres tipos de alertas alert, confirm,prompt//
+alert('SE CONFIRMARA SU TRANSACCION');
+var conf = confirm('ACEPTA LAS POLIICAS DE SERVICIO');
+var prop = prompt('SE AGRAGARA EL ITEM');
+console.log(prop);
+console.log(conf);
